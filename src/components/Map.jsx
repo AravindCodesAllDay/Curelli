b4b856770ff54cf3b1578cb8b86a4411ca57ffc7
AravindCodesAllDay <aravindsiva2003.in@gmail.com>
2024-03-14 19:44:48 +0530

    map2

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,11 +4,11 @@ const Map = () => {
   return (
     <div className="">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d563.9312520876067!2d77.46827766450136!3d8.214861761797103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04f7680be7475f%3A0x3127d3ad1c005aff!2sSahayanagar!5e0!3m2!1sen!2sin!4v1710421918872!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3948.8660361261777!2d77.46694911126795!3d8.216224741782069!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04f700429da07b%3A0xbf7da2c0e8617c4e!2sCurelli%20Foods!5e0!3m2!1sen!2sin!4v1710422544830!5m2!1sen!2sin"
+        className="border-0 w-screen h-96"
         allowfullscreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
-        className="border-0 w-screen h-96"
       ></iframe>
     </div>
   );
